7f336fe02631b785476d55dcdc0438fe3c403fa0
irfanmanzoor7760 <irfanmanzoor7760@gmail.com>
2025-10-01 15:36:11 +0500

    test plates

--- plates.py
+++ plates.py
@@ -1,31 +1,17 @@
-def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
-
-
-def is_valid(s):
-    if 2 <= len(s) <= 6 and s.isalnum():
-        # Return true if characters are all letters
-        if s.isalpha():
-            return True
-        else:
-            # Check for number in the middle
-            # (only if the first two characters are letters and the last character is number)
-            if s[:2].isalpha() and s[-2:].isdigit():
-                for i in range(len(s)):
-                    if s[i].isdigit():
-                        # Return false if number starts with 0 or the following character is letter
-                        if s[i].startswith("0") or s[i:].isalpha():
-                            return False
-                        else:
-                            return True
-            else:
-                return False
-    else:
+def is_valid(plate):
+    if len(plate) < 2 or len(plate) > 6:
         return False
 
+    if not plate.isalnum():
+        return False
 
-main()
+    if not plate[:2].isalpha():
+        return False
+
+    if not plate[-1].isdigit() or plate.startswith("0"):
+        return False
+
+    if any(char in ". ,;:!?@#$%^&*()-_=+{}[]|<>/`~" for char in plate):
+        return False
+
+    return True
